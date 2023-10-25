8821410bd799a2c7c4a718cdbf9812de6576ffaa
Nestor Larenas <sne.larenasortega@hotmail.com>
2023-10-25 12:24:48 -0300

    LINQ Practice

---
+++ Product.cs
@@ -0,0 +1,7 @@
+#pragma warning disable CS8618
+public class Product
+{
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public double Price { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Product[] myProducts = new Product[]
+        {
+            new Product { Name = "Jeans", Category = "Clothing", Price = 24.7 },
+            new Product { Name = "Socks", Category = "Clothing", Price = 8.12 },
+            new Product { Name = "Scooter", Category = "Vehicle", Price = 99.99 },
+            new Product { Name = "Skateboard", Category = "Vehicle", Price = 24.99 },
+            new Product { Name = "Skirt", Category = "Clothing", Price = 17.5 }
+        };
+
+        IEnumerable<Product> sortProductsByPrice =
+            myProducts.OrderByDescending(prod => prod.Price);
+
+        IEnumerable<Product> orderedClothingProducts =
+            myProducts.Where(c => c.Category == "Clothing").OrderByDescending(prod => prod.Price);
+
+        Console.WriteLine("Productos ordenados por precio:");
+        foreach (var product in sortProductsByPrice)
+        {
+            Console.WriteLine($"Nombre: {product.Name}, Categoría: {product.Category}, Precio: {product.Price}");
+        }
+
+        Console.WriteLine("\nProductos de ropa ordenados por precio:");
+        foreach (var product in orderedClothingProducts)
+        {
+            Console.WriteLine($"Nombre: {product.Name}, Categoría: {product.Category}, Precio: {product.Price}");
+        }
+    }
+}
